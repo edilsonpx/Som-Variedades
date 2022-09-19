23e7cba439695fdcc2e1c285de81569000529cc3
edilson <94394633+edilsonpx@users.noreply.github.com>
2022-09-18 21:22:23 -0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,60 @@
+
+var thumb1 =  document.getElementById("thumb1");
+var thumb1Src =  document.getElementById("thumb1").src;
+var thumb2 =  document.getElementById("thumb2");
+var thumb2Src =  document.getElementById("thumb2").src;
+var thumb3 =  document.getElementById("thumb3");
+var thumb3Src =  document.getElementById("thumb3").src;
+var thumb4 =  document.getElementById("thumb4");
+var thumb4Src =  document.getElementById("thumb4").src;
+var thumb5 =  document.getElementById("thumb5");
+var thumb5Src =  document.getElementById("thumb5").src;
+var thumb6 =  document.getElementById("thumb6");
+var thumb6Src =  document.getElementById("thumb6").src;
+
+
+thumb1.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb1Src
+})
+thumb2.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb2Src
+})
+thumb3.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb3Src
+})
+thumb4.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb4Src
+})
+thumb5.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb5Src
+})
+thumb6.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb6Src
+})
+
+
+
+
+var thumb11 =  document.getElementById("thumb11");
+var thumb11Src =  document.getElementById("thumb11").src;
+var thumb22 =  document.getElementById("thumb22");
+var thumb22Src =  document.getElementById("thumb22").src;
+var thumb33 =  document.getElementById("thumb33");
+var thumb33Src =  document.getElementById("thumb33").src;
+var thumb44 =  document.getElementById("thumb44");
+var thumb44Src =  document.getElementById("thumb44").src;
+
+thumb11.addEventListener("click",function(){
+	document.getElementById("mainImg1").src = thumb11Src
+})
+thumb22.addEventListener("click",function(){
+	document.getElementById("mainImg1").src = thumb22Src
+})
+thumb33.addEventListener("click",function(){
+	document.getElementById("mainImg1").src = thumb33Src
+})
+
+thumb44.addEventListener("click",function(){
+	document.getElementById("mainImg1").src = thumb44Src
+
+})
